67d9a2fb4e37d777104c60df3ee6ddecf22f932a
Yuriy Bezsonov <ybezsonov@gmail.com>
2025-01-30 12:33:23 +0100

    test dataSource.flush

--- src/main/java/com/unicorn/store/data/CheckpointUtil.java
+++ src/main/java/com/unicorn/store/data/CheckpointUtil.java
@@ -1,25 +1,33 @@
 package com.unicorn.store.data;
 
-// import io.agroal.api.AgroalDataSource;
 import org.crac.Context;
 import org.crac.Core;
 import org.crac.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// import jakarta.inject.Inject;
+import io.agroal.api.AgroalDataSource;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import io.agroal.api.AgroalDataSource.FlushMode;
 
 public class CheckpointUtil implements Resource, ServletContextListener {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    // @Inject
-    // AgroalDataSource dataSource;
+    @Inject
+    AgroalDataSource dataSource;
+
+    public void contextInitialized(ServletContextEvent event) {
+        Core.getGlobalContext().register(this);
+    }
 
     @Override
     public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
         logger.info("Preparing for checkpoint ...");
+        dataSource.flush(FlushMode.ALL);
         // dataSource.close();
+        logger.info("Checkpoint prepared ...");
     }
 
     @Override
